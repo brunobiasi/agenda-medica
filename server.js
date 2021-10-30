930b77b283d02273d67a3dac4c5f73025942fb88
Bruno Biasi <brunobiasi32@gmail.com>
2021-10-30 14:24:00 -0300

    conectei com o banco de dados

--- server.js
+++ server.js
@@ -2,14 +2,24 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+mongoose.connect('mongodb://localhost:27017/agenda-medica', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}, (err) => {
+    if(err){
+        console.log(err);
+    } else {
+        console.log("MongoDB CONECTADO com sucesso!");
+    }
+});
+
 app.use(cors());
-
 app.use(cookieParser());
-
 app.use(express.json());
 
 app.get('/', (req, res) => {

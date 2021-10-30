38ad0aa1c29988d742f1f113ab94bad9ded45c91
Bruno Biasi <brunobiasi32@gmail.com>
2021-10-30 13:34:29 -0300

    criei o servidor

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({message: "Hello World"});
+});
+
+app.listen(port, () => {
+    console.log(`Server runing on port ${port}`);
+});

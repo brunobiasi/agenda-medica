0eaf1fd3df618ac17141d9edd4ad529c7d115ab0
Bruno Biasi <brunobiasi32@gmail.com>
2021-11-29 17:06:47 -0300

    logout no sistema com reactjs e nodejs

--- src/routes.js
+++ src/routes.js
@@ -15,6 +15,7 @@ routes.delete('/api/usuarios/:_id', Usuario.delete);
 routes.put('/api/usuarios', Usuario.update);
 routes.post('/api/usuarios/login', Usuario.login);
 routes.get('/api/usuarios/checktoken', Usuario.checkToken);
+routes.get('/api/usuarios/destroytoken', Usuario.destroyToken);
 
 // Rotas de Agendamentos
 routes.post('/api/agendamentos', Agendamento.create);
